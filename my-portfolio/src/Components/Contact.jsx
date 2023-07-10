32e80688c9829902279804cdcd22a83c445f1128
devgithubsharma <devsharmabsr03@gmail.com>
2023-07-10 22:10:45 +0530

    changes

--- my-portfolio/src/Components/Contact.jsx
+++ my-portfolio/src/Components/Contact.jsx
@@ -4,14 +4,14 @@ const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
       <form method='POST' action='https://getform.io/f/6e25d0a6-daff-4de9-bd4b-dd1a4f6cc107' className='flex flex-col max-w-[600px] w-full'>
-        <div className='pb-4'>
-            <p className='text-3xl font-bold inline border-b-4 border-rose-600 text-gray-100'> Contact</p>
+        <div className='pb-2'>
+            <p className='text-2xl font-bold inline border-b-4 border-rose-600 text-gray-100'> Contact</p>
             <p className='text-gray-100 py-2'>Interested to Know more ? <br/> Contact me on - devsharmabsr03@gmail.com</p>
         </div>
         <input className='bg-[#eeeff3] p-2' type="text" placeholder='Name' name='name'></input>
         <input className='bg-[#eeeff3] p-2 my-4' type="email" placeholder='Email' name='email'></input>
         <textarea className='bg-[#eeeff3] p-2' rows='10' placeholder='Message' name='message'></textarea>
-        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center '>
+        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 my-8 mx-auto flex items-center '>
             Let's Collaborate
         </button>   
       </form>
--- my-portfolio/src/Components/Skills.jsx
+++ my-portfolio/src/Components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-100'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-            <p className='text-3xl font-bold inline border-b-4 border-rose-600'>Skills</p>
+            <p className='text-2xl font-bold inline border-b-4 border-rose-600'>Skills</p>
             <p className='py-2'>These are the Technology stacks, I've worked with !</p>
         </div>
 
