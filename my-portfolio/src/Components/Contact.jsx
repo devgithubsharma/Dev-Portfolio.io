4e0912b6b42ae14e53785f70eaf7f19d10b73a80
devgithubsharma <devsharmabsr03@gmail.com>
2023-07-10 21:26:57 +0530

    changes

--- my-portfolio/src/Components/Contact.jsx
+++ my-portfolio/src/Components/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
       <form method='POST' action='https://getform.io/f/6e25d0a6-daff-4de9-bd4b-dd1a4f6cc107' className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-rose-600 text-gray-100'> Contact</p>
-            <p className='text-gray-100 py-4 text-2xl'>Interested to Know more ? <br/> Contact me on - devsharmabsr03@gmail.com</p>
+            <p className='text-3xl font-bold inline border-b-4 border-rose-600 text-gray-100'> Contact</p>
+            <p className='text-gray-100 py-3 text-2xl'>Interested to Know more ? <br/> Contact me on - devsharmabsr03@gmail.com</p>
         </div>
         <input className='bg-[#eeeff3] p-2' type="text" placeholder='Name' name='name'></input>
         <input className='bg-[#eeeff3] p-2 my-4' type="email" placeholder='Email' name='email'></input>
--- my-portfolio/src/Components/Skills.jsx
+++ my-portfolio/src/Components/Skills.jsx
@@ -18,14 +18,14 @@ const Skills = () => {
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-            <p className='text-4xl font-bold inline border-b-4 border-rose-600'>Skills</p>
-            <p className='py-4 text-2xl'>These are the Technology stacks, I've worked with !</p>
+            <p className='text-3xl font-bold inline border-b-4 border-rose-600'>Skills</p>
+            <p className='py-3 text-2xl'>These are the Technology stacks, I've worked with !</p>
         </div>
 
-        <div className='w-full grid grid-cols-3 sm:grid-cols-4 gap-4 text-center py-8'>
+        <div className='w-full grid grid-cols-3 sm:grid-cols-4 gap-2 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src={Html} alt='' />
-                <p className='my-4  font-bold'>HTML</p>
+                <p className='my-4    font-bold'>HTML</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src={Css} alt='' />
